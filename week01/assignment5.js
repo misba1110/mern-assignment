8d703ba0a2af09a7db08df1b3f13804e9fd99b1d
misba1110 <misba.hunnisa@mfine.co>
2021-08-23 01:53:17 +0530

    my commit

---
+++ week01/assignment5.js
@@ -0,0 +1,20 @@
+function onsubmit(){
+    var data=read();
+
+}
+function read(){
+    var data={};
+   
+    data["title"]=document.getElementById("title").Value;
+ data["author"]=document.getElementById("title").Value;
+ data["price"]=document.getElementById("title").Value;
+ return data;
+}
+function insert(data)
+{
+var table=document.getElementById("booklist");
+var items=document.getElementsByName("tbody");
+var book=[];
+
+
+}
